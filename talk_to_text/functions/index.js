a96e172d92da9ffa27c89405f1704d0fc0b8f3d6
seungc1 <seungc1114@gmail.com>
2025-04-03 20:07:58 +0900

    배포지 next.js페이지 표시 완료

--- talk_to_text/functions/index.js
+++ talk_to_text/functions/index.js
@@ -2,9 +2,15 @@ const functions = require('firebase-functions');
 const next = require('next');
 
 const isDev = process.env.NODE_ENV !== 'production';
-const app = next({ dev: isDev, conf: { distDir: '.next' } });
+const app = next({
+  dev: isDev,
+  conf: {
+    distDir: '.next' // Next.js 빌드 결과 디렉토리
+  }
+});
 const handle = app.getRequestHandler();
 
-exports.nextApp = functions.https.onRequest((req, res) => {
-  return app.prepare().then(() => handle(req, res));
+exports.nextApp = functions.https.onRequest(async (req, res) => {
+  await app.prepare(); // 빌드 준비
+  return handle(req, res); // 요청 라우팅 처리
 });
