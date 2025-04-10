daec82a5372d46b045a1b7ff2edbc10bb9f0cdce
seungc1 <seungc1114@gmail.com>
2025-04-10 13:10:35 +0900

    npm run dev 문제 해결

--- talk_to_text/functions/.next/types/app/layout.ts
+++ talk_to_text/functions/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/user/group-project/group-project/talk_to_text/app/layout.js
+// File: /Users/user/group-project/group-project/talk_to_text/functions/app/layout.js
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
